049b1cff004ab09d3a955a56e4cce1837b51e9bc
Eric Chiu <eric.chiu@millionasia.com>
2026-05-28 03:12:20 +0800

    Fix Vercel Analytics client setup

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
-<script setup>
-import { Analytics } from '@vercel/analytics/nuxt/runtime'
-</script>
-
 <template>
   <div class="min-h-screen bg-mist text-ink">
-    <Analytics />
     <AppHeader />
     <main>
       <NuxtPage />
